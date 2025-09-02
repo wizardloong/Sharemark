c489b73bf57d1e2b3a08576d9f266852d6bfa336
Victor Miroliubov <w.izard@outlook.com>
2025-09-02 18:55:34 +0300

    try to add middleware to fix http issue

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ from pathlib import Path
 from fastapi_limiter import FastAPILimiter
 from fastapi.middleware.trustedhost import TrustedHostMiddleware
 from fastapi.middleware.gzip import GZipMiddleware
+from starlette.middleware.proxy_headers import ProxyHeadersMiddleware
 
 # Загружаем .env
 load_dotenv(dotenv_path=Path(__file__).parent.parent / "env" / ".env")
@@ -49,6 +50,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(GZipMiddleware)
 app.add_middleware(TrustedHostMiddleware, allowed_hosts=[os.getenv("DOMAIN_NAME"), "localhost"])
+app.add_middleware(ProxyHeadersMiddleware, trusted_hosts="*")  # 👈 важно!
 
 app.mount("/static", StaticFiles(directory="public/static"), name="static")
 
